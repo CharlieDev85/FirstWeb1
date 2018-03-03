499897f7c48e322d4a3e96054766ddd6d5e562fa
CharlieDev <php.developer.charlie@gmail.com>
2018-03-03 08:10:35 -0600

    change servlet deployment descriptors

--- src/com/HelloWorld/servlets/HelloWorld.java
+++ src/com/HelloWorld/servlets/HelloWorld.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloWorld
  */
-@WebServlet("/HelloWorld")
+@WebServlet("/hiddenservlets/helloworld2.do")
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
